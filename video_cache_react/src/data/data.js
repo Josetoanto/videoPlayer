d501ef8a2755d9fa405b34ecddd4e5bec0a5d33e
Josetoanto <jarf1369@gmail.com>
2025-04-04 22:56:26 -0600

    AplicacionTerminada

---
+++ video_cache_react/src/data/data.js
@@ -0,0 +1,40 @@
+const moviesData = [
+  
+  {
+    id: 11,
+    title: "Escandalosos",
+    genre: "Comedy",
+    imageUrl: "https://i.pinimg.com/736x/e6/7a/d6/e67ad61fa73443e5bcb25bc17c31f063.jpg",
+    video: "https://spontaneity-2025.s3.us-east-1.amazonaws.com/Aventura+culinaria+Escandalosos+Cartoon+Network.mp4"
+  },
+ 
+  {
+    id: 13,
+    title: "Hora de Aventura: Tierras Lejanas",
+    genre: "Adventure",
+    imageUrl: "https://m.media-amazon.com/images/M/MV5BZWU0Njc1MzYtNDAzMC00MGZhLThhYTMtNDllYzUxNTMzOGQ0XkEyXkFqcGc@._V1_.jpg",
+    video: "https://spontaneity-2025.s3.us-east-1.amazonaws.com/When+Football+Tries+to+Steal+BMO's+Body+Adventure+Time+Cartoon+Network.mp4"
+  },
+  {
+    id: 6,
+    title: "The Loud House",
+    genre: "Comedy",
+    imageUrl: "https://m.media-amazon.com/images/I/81mWbI6gvnL._AC_UF894,1000_QL80_.jpg",
+    video: "https://spontaneity-2025.s3.us-east-1.amazonaws.com/The+Loud+House+Extended+Official+Opening+Theme+Song.mp4"
+  },
+  {
+    id: 12,
+    title: "Rick y Morty",
+    genre: "Comedy",
+    imageUrl: "https://m.media-amazon.com/images/M/MV5BZGQyZjk2MzMtMTcyNC00NGU3LTlmNjItNDExMWM4ZDFhYmQ2XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    video: "https://spontaneity-2025.s3.us-east-1.amazonaws.com/Pepinillo+Rick+_+Rick+y+Morty+_+HBO+Max.mp4a"
+  },
+  {
+    id: 8,
+    title: "Phineas y Ferb",
+    genre: "Comedy",
+    imageUrl: "https://images.justwatch.com/poster/317405677/s718/take-two-with-phineas-and-ferb.jpg",
+    video: "https://spontaneity-2025.s3.us-east-1.amazonaws.com/%C3%9Altimo+D%C3%ADa+de+Verano+Phineas+y+Ferb.mp4"
+  }
+];
+export default moviesData;
